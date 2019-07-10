4e07a94dd50b0c4fcbb7393a1d5d8b98c938116e
Adnan <52744826+Adnant35@users.noreply.github.com>
2019-07-10 14:45:39 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package JavaApplication1;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+         
+      
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new javaapplication1.Giris().setVisible(true); }
+        });
+        
+        
+    }
+
+    
+             }
